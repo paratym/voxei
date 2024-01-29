bf129b8bc572f2b9f7f7438b57cf39dd1da4a0f6
pengiie <nathan@pengie.dev>
2024-01-28 20:10:55 -0700

    interesting effect, exit_axis coloring

--- shaders/lib/intersect.glsl
+++ shaders/lib/intersect.glsl
@@ -12,3 +12,19 @@ vec2 ray_box_intersection(Ray ray, vec3 box_min, vec3 box_max) {
 
   return vec2(tenter, texit);
 }
+
+vec2 ray_box_intersection_extra(Ray ray, vec3 box_min, vec3 box_max, out vec3 tmin, out vec3 tmax) {
+  vec3 t0 = (box_min - ray.origin) * ray.inv_dir;
+  vec3 t1 = (box_max - ray.origin) * ray.inv_dir;
+  tmin = min(t0, t1);
+  tmax = max(t0, t1);
+
+  vec2 traverse = max(tmin.xx, tmin.yz);
+  float tenter = max(traverse.x, traverse.y);
+
+  traverse = min(tmax.xx, tmax.yz);
+  float texit = min(traverse.x, traverse.y);
+
+  return vec2(tenter, texit);
+}
+
--- shaders/voxel.comp.glsl
+++ shaders/voxel.comp.glsl
@@ -32,7 +32,7 @@ layout (set = 0, binding = 4) uniform VoxelInfo {
   uint grid_length;
 } voxel_info;
 
-const uint SUBDIVISIONS = 3;
+const uint SUBDIVISIONS = 2;
 
 #include "lib/morton.glsl"
 #include "lib/voxel.glsl"
@@ -55,9 +55,9 @@ float get_half_length(uint depth) {
 // eg. ray_box_intersection(ray, vec3(0.0), 0)
 // with subdivisions of 3 would be calculating the bounds
 // of a min (-4, -4, -4) and max (4, 4, 4) box
-vec2 ray_voxel_intersection(Ray ray, vec3 pos, uint depth) {
+vec2 ray_voxel_intersection(Ray ray, vec3 pos, uint depth, out vec3 tmin, out vec3 tmax) {
   float half_length = get_half_length(depth);
-  return ray_box_intersection(ray, pos - half_length, pos + half_length);
+  return ray_box_intersection_extra(ray, pos - half_length, pos + half_length, tmin, tmax);
 }
 
 // Returns the 3 digit morton code for the child voxel local position
@@ -106,17 +106,17 @@ vec3 get_child_position(vec3 parent_pos, uint child_local_morton, uint child_dep
   float half_length = get_half_length(child_depth);
   vec3 child_mask = vec3(
     (child_local_morton & 1) > 0 ? 1.0 : -1.0,
-    (child_local_morton & 2) > 0 ? 1.0 : -1.0,
-    (child_local_morton & 4) > 0 ? 1.0 : -1.0
+    (child_local_morton & 2) > 0 ? -1.0 : 1.0,
+    (child_local_morton & 4) > 0 ? -1.0 : 1.0
   );
-  return parent_pos + (child_mask * half_length);
+  return parent_pos + (child_mask * half_length * 0.5);
 }
 
 uint get_child_node_index(uint parent_node_index, uint child_local_morton) {
   VoxelNode parent_node = voxel_nodes[parent_node_index];
   uint child_node_index = parent_node.child_index;
   if(child_node_index == 0) {
-    return 0;
+    return 1;
   }
   uint grouped_child_offset = parent_node.child_offsets[child_local_morton / 4];
   // Since the offset is stored as 2xu32, we need to interpret it as 8xu8 
@@ -133,7 +133,12 @@ struct TraceOutput {
   vec3 color;
 };
 
-TraceOutput trace(Ray ray, out vec3 pos) {
+struct StackItem {
+  uint node_index;
+  vec3 pos;
+};
+
+TraceOutput trace(Ray ray) {
   vec2 root_t_corners = ray_box_intersection(ray, voxel_info.bbmin, voxel_info.bbmax);
   bool hit = root_t_corners.y > max(root_t_corners.x, 0.0);
   if(!hit) {
@@ -145,47 +150,100 @@ TraceOutput trace(Ray ray, out vec3 pos) {
   vec3 root_pos = voxel_info.bbmin + ((voxel_info.bbmax - voxel_info.bbmin) * 0.5);
   vec3 root_t_mid = (root_pos - ray.origin) * ray.inv_dir;
   uint first_child_local_morton = get_child_local_morton(ray, root_t_mid, root_t_corners);
-  vec3 first_child_pos = get_child_position(root_pos, first_child_local_morton, 1);
-
-  vec3 color = vec3(0.3);
+  vec3 first_child_pos = get_child_position(root_pos, first_child_local_morton, 0);
 
   uint depth = 1u;
-  pos = first_child_pos;
-  uint local_morton = first_child_local_morton;
+  vec3 pos = first_child_pos;
+  uint morton = first_child_local_morton;
   uint parent_node_index = voxel_node_len - 1;
-  float t_exit = root_t_corners.y;
-  for(uint i = 0; i < 128; i++) {
+  // Holds parent indices
+  StackItem stack[SUBDIVISIONS + 1];
+  stack[1] = StackItem(parent_node_index, pos);
+
+  bool dont_push = false;
+  vec3 color = vec3(0.2);
+  for(uint i = 0; i < 3; i++) {
     // Calculate the intersection of the ray with the current voxel
-    vec2 tc = ray_voxel_intersection(ray, pos, depth);
+    vec3 tmin, tmax;
+    vec2 tc = ray_voxel_intersection(ray, pos, depth, tmin, tmax);
     bool hit = tc.y > max(tc.x, 0.0);
-
-    // Visualization
-    color += vec3(0.1);
-
+    uint local_morton = morton & 7;
     if(hit) {
-      // Check if the voxel is a leaf
       uint node_index = get_child_node_index(parent_node_index, local_morton);
-      if(node_index != 0) {
+      if(node_index != 0 && !dont_push) {
         VoxelNode child = voxel_nodes[node_index];
-        if(depth == SUBDIVISIONS) {
-          color = vec3(1.0, 0.0, 0.0);
+
+        if (child.data_index != 0) {
+          color = vec3(0.0, 1.0, 0.0);
           break;
         }
 
-        if(child.data_index != 0) {
-          // We have hit a leaf node
-          color = vec3(1.0);
-          break;
-        }
+        stack[depth] = StackItem(parent_node_index, pos);
 
         parent_node_index = node_index;
         vec3 t_mid = (pos - ray.origin) * ray.inv_dir;
-        local_morton = get_child_local_morton(ray, t_mid, tc);
-        pos = get_child_position(pos, local_morton, depth);
+        uint child_morton = get_child_local_morton(ray, t_mid, tc);
+        pos = get_child_position(pos, child_morton, depth);
+        morton = (morton << 3) | child_morton;
         depth++;
+        continue;
       }
     } else {
       break;
+    }
+
+    // We no longer have a valid child node so we are in empty space and need to advance
+    // our ray to the next voxel, we do this by finding which axis the ray exits the current voxel on,
+    // calculate the flipped bit in the local morton code, and compare that against the ray direction
+    // to determine if we are no longer in the same parent. 
+
+    // Example: a ray going from -x to +x and advancing from 01 to 00 will have a flipped bit in the x axis, 
+    // since the ray is increasing and the code decreased, we know we are no longer in the same parent.
+
+    // Check which axis we exit the voxel on
+    bvec3 exit_axis = bvec3(
+      tc.y == tmax.x,
+      tc.y == tmax.y,
+      tc.y == tmax.z
+    );
+
+    color = vec3(exit_axis.x ? 1.0 : 0.0, exit_axis.y ? 1.0 : 0.0, exit_axis.z ? 1.0 : 0.0);
+
+    vec3 ones = vec3(
+      (morton & 1) > 0 ? 0 : 1,
+      (morton & 2) > 0 ? 0 : 1,
+      (morton & 4) > 0 ? 0 : 1
+    );
+
+    morton = (morton) ^ (exit_axis.x ? 1 : 0) | (exit_axis.y ? 2 : 0) | (exit_axis.z ? 4 : 0);
+
+    // xor the exit axis with the ones to get the flipped bits
+    vec3 new_ones = vec3(
+      (morton & 1) > 0 ? 0 : 1,
+      (morton & 2) > 0 ? 0 : 1,
+      (morton & 4) > 0 ? 0 : 1
+    );
+    pos += sign(ray.dir) * vec3(exit_axis) * get_half_length(depth) * 2;
+
+    // Check if the ray is increasing or decreasing corresponding to the flipped bits
+    bool against_ray_dir = true;
+    if(exit_axis.x) {
+      against_ray_dir = ray.dir.x > 0.0;
+    } else if(exit_axis.y) {
+      against_ray_dir = ray.dir.y > 0.0;
+    } else if(exit_axis.z) {
+      against_ray_dir = ray.dir.z > 0.0;
+    }
+
+    if(against_ray_dir) {
+      depth--;
+      StackItem item = stack[depth];
+      parent_node_index = item.node_index;
+      pos = item.pos;
+      morton = morton >> 3;
+      dont_push = true;
+    } else {
+      dont_push = false;
     }
   }
 
@@ -205,8 +263,7 @@ void main() {
   vec3 ro = vec3(vec4(0.0, 0.0, 0.0, 1.0) * camera.view);
   vec3 rd = normalize(vec3(scaled_uv, 1.0)) * mat3(camera.view);
 
-  vec3 pos;
-  TraceOutput trace_out = trace(Ray(ro, rd, 1.0 / rd), pos);
+  TraceOutput trace_out = trace(Ray(ro, rd, 1.0 / rd));
 
   vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
 
